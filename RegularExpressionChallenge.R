4d6ef4bf06b7083f8e072135538befd4ace8ac53
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-01-30 14:01:03 +0000

    Create a shorter gsub command

--- RegularExpressionChallenge.R
+++ RegularExpressionChallenge.R
@@ -6,7 +6,6 @@ grep_out <- grep(pattern='C', x=species)
 species[grep_out]
 grep_out2 <- grep(pattern='_r', x=species)
 species[grep_out2]
-gsub_out <- gsub(pattern='Copis', x=species, replacement='Copris')
-gsub_out
-gsub_out <- gsub(pattern='Microcopis', x=species, replacement='Microcopris')
+gsub_out <- gsub('Copis', 'Copris',
+            gsub('Microcopis', 'Microcopris', x=species))
 gsub_out
