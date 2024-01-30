fabd4ae0b6e3841a7904ff4130b0c8465ed35025
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-01-30 13:15:02 +0000

    Search for characters in a sentence and use quantifiers

--- RegularExpressions.R
+++ RegularExpressions.R
@@ -5,3 +5,12 @@ sentence[grep_out]
 grep2 <- grep(pattern='^on$', x=sentence)
 grep2
 sentence[grep2]
+grep_out <- grep(pattern = '[A-Z]', x = sentence) #Searches for capital letters in the sentence
+grep_out
+grep_out <- grep(pattern = 'a.e', x = sentence) #Searches for a and e separated by any characters
+sentence[grep_out]
+grep3 <- grep(pattern = '[A-z]', x=sentence) #Searches for all capital and lowercase characters
+sentence[grep3]
+sentence[grep(pattern = 'e.?e', x = sentence)] #? makes a pattern optional (matches 0 or 1 times)
+sentence[grep(pattern = 'e.*e', x = sentence)] #* lets a pattern be optional or repeat (matches any number of times, including 0)
+sentence[grep(pattern = 'e.+e', x = sentence)] #+ lets a pattern repeat (matches at least once)
